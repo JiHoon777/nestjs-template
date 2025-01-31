4e7ef20c7f7a7ea5422fa7a34edc8c1ac10fd46b
지훈 송 <partner.chase@kyobobook.com>
2025-02-01 00:21:08 +0900

    feat: api prefix , versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AllExceptionsFilter } from '@shared/filters'
 import { TransformInterceptor } from '@shared/interceptors'
@@ -7,6 +7,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  })
 
   app.useLogger(['error', 'warn', 'log', 'debug', 'verbose'])
   app.useGlobalFilters(new AllExceptionsFilter())
